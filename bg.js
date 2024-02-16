090ddacf68569bc799616a5577b38cb3aa18b5df
opDavi1 <121784309+opDavi1@users.noreply.github.com>
2024-02-15 17:07:36 -0800

    Fixed background img css and added 2 papes

--- bg.js
+++ bg.js
@@ -1,16 +1,33 @@
+const RANDOM_BG_ENABLED = true;
+const DEFAULT_BG = 6;
+
+/////////////////////////////////////////////
+
 const bg_list = 
     [
         'dark-souls.jpg',
         'highway_nighttime.png',
         'Nerv.png',
-        'nighttime-anime-girl',
+        'nighttime-anime-girl.jpg',
         'rei.png',
         'rip-brian.jpg',
         'saturn.png',
         'smoking-anime-girl.jpg',
         'space-cowboy.jpg',
         'spring-path.jpg',
-        'sunset-train-station.jpg'
+        'sunset-train-station.jpg',
+        'girls-last-tour.jpg',
+        'femboys.png'
     ];
-let index = Math.floor(Math.random() * bg_list.length);
-document.body.style.backgroundImage = `url(./bg/${bg_list[index]})`;
+
+function setBg(i) {
+    document.body.style.backgroundImage = `url(./bg/${bg_list[i]})`;
+}
+
+if(RANDOM_BG_ENABLED) {
+    let index = Math.floor(Math.random() * bg_list.length);
+    setBg(index);
+} else {
+    setBg(DEFAULT_BG);
+}
+
